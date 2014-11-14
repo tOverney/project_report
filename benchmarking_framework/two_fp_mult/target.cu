43a1e07171f3dac833aa6fa2ffffd9dc5fbf66a5
Clement Humbert <clement.humbert@epfl.ch>
2014-11-14 09:50:24 +0100

    removed benchmark dir

--- benchmarking_framework/two_fp_mult/target.cu
+++
@@ -1,2 +0,0 @@
-asm volatile("mul.OPERAND_TYPE %0, %0, %1;" : "+CONSTRAINT_LETTER"(op_a) : "CONSTRAINT_LETTER"(op_b));
-asm volatile("mul.OPERAND_TYPE %0, %0, %1;" : "+CONSTRAINT_LETTER"(op_a) : "CONSTRAINT_LETTER"(op_b));
