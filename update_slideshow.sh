7cdafd257f92d9c73e7f2e478354531688cec816
tOverney <tristan.overney@epfl.ch>
2014-11-17 14:02:11 +0100

    Automatic update of the slideshow

--- update_slideshow.sh
+++ update_slideshow.sh
@@ -4,8 +4,12 @@ cd sliderust-master/
 ./build.sh
 cd ..
 
+git add .
+git commit -m $1
+git push master
+
 git checkout gh-pages
 git checkout master sliderust-master/
 git commit -m "Automatic update of the slideshow"
-git push -f
+git push -f gh-pages
 git checkout master
