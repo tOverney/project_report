8448ea241beef28311d8bb1862bd26c545015d06
tOverney <tristan.overney@epfl.ch>
2014-11-20 14:29:28 +0100

    Super-commit convert .pdf to .png

--- super_commit.sh
+++ super_commit.sh
@@ -10,6 +10,12 @@ git push origin master
 
 git checkout gh-pages
 git checkout master sliderust-master/ pictures/ graphics/
+
+cd graphics/
+mogrify -format png *.pdf
+rm *.png
+cd ..
+
 git commit -m "Automatic update of the slideshow"
 git push -f origin gh-pages
 git checkout master
