a8feb32f4ce8200d2d7fcb09f9a87e39a6da4366
Tristan David Overney <tdoverne@lappc63.epfl.ch>
2014-12-04 15:05:03 +0100

    stupid ---

--- benchmark_simulations/fp_mul_500-500/target.cu
+++ benchmark_simulations/fp_mul_500-500/target.cu
@@ -1,1 +1,2 @@
 asm volatile("mul.OPERAND_TYPE %0, %0, %1;" : "+CONSTRAINT_LETTER"(op_a) : "CONSTRAINT_LETTER"(op_b));
+asm volatile("mul.OPERAND_TYPE %0, %0, %1;" : "+CONSTRAINT_LETTER"(op_c) : "CONSTRAINT_LETTER"(op_d));
